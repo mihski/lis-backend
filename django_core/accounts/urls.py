f3a5d107180cb9c0fb7273a00beca0b77d0fcf9c
EgorovM <egorov_michil@mail.ru>
2022-08-24 16:11:05 +0300

    [feat] user loginning while editor session existing

--- django_core/accounts/urls.py
+++ django_core/accounts/urls.py
@@ -8,3 +8,26 @@ router = SimpleRouter()
 router.register('users', UserViewSet)
 
 urlpatterns = router.urls
+
+
+# Override only one auth for one account
+
+from django.contrib import admin
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+from editors.models import EditorSession
+
+
+class UniqueAccountAuthForm(AuthenticationForm):
+    def confirm_login_allowed(self, user):
+        super().confirm_login_allowed(user)
+
+        # TODO: add is_closed
+        if EditorSession.objects.filter(user=user).exists():
+            raise ValidationError(
+                "Пользователь еще не закончил сессию в редакторе. Во избежание проблем, мы не можем вас авторизовать."
+            )
+
+
+admin.autodiscover()
+admin.site.login_form = UniqueAccountAuthForm
