8b7cd46dd026134a06b6ae3f64cf182ec7b1ebe6
EgorovM <egorov_michil@mail.ru>
2022-09-01 16:05:15 +0300

    [feat] forbid deleting editorsession

--- django_core/editors/admin.py
+++ django_core/editors/admin.py
@@ -12,5 +12,7 @@ class EditorSessionAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
 
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 admin.site.register(models.EditorSession, EditorSessionAdmin)
