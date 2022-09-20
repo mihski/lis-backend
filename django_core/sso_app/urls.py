5d847d4ab9e9eae3476ffd9716742805ad789cb3
EgorovM <egorov_michil@mail.ru>
2022-09-20 11:29:04 +0300

    [feat] test authorization

--- django_core/sso_app/urls.py
+++ django_core/sso_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from sso_app.views import RedirectISU, GetISUToken
+from sso_app.views import RedirectISU, GetISUToken, RedirectISUTest, GetISUTokenTest
 
 app_name = 'sso_app'
 
 urlpatterns = [
     path('', RedirectISU.as_view(), name='redirect_isu'),
-    path('redirect/', GetISUToken.as_view(), name='get_isu_token')
+    path('create/', GetISUToken.as_view(), name='get_isu_token'),
+    path('test/', RedirectISUTest.as_view(), name='test_redirect_isu'),
+    path('test/create/', GetISUTokenTest.as_view(), name='test_get_isu_token'),
 ]
--- django_core/sso_app/views.py
+++ django_core/sso_app/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from django.http import HttpResponseRedirect
 from sso_app.auth_isu import ISUManager
 
+from accounts.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 class RedirectISU(APIView):
     """ Вьюшка для редиректа на CAS авторизацию.
@@ -12,6 +15,14 @@ class RedirectISU(APIView):
         isu_manager = ISUManager()
 
         return HttpResponseRedirect(isu_manager.obtain_auth_url())
+
+
+class RedirectISUTest(APIView):
+    """ Вьюшка для теста редиректа на CAS авторизацию.
+    Симулирует возвращение code.
+    """
+    def get(self, request):
+        return HttpResponseRedirect('/auth/?code=ASDNKJE83jS')
 
 
 class GetISUToken(APIView):
@@ -24,4 +35,29 @@ class GetISUToken(APIView):
         return Response({
             'refresh_token': refresh_token,
             'access_token': access_token,
+        })
+
+
+class GetISUTokenTest(APIView):
+    """ Вьюшка для получения токенов пользователя по коду авторазции
+    Возвращает токены для тестового пользователя
+    """
+
+    def get(self, request):
+        api_test_username = 'api_test'
+
+        user = User.objects.filter(username=api_test_username).first()
+
+        if not user:
+            user = User.objects.create_user(
+                username=api_test_username,
+                email='api_test@mail.ru',
+                password='password',
+            )
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'refresh_token': str(refresh),
+            'access_token': str(refresh.access_token),
         })
