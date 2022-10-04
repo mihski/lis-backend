59ded4a14576f9b30e0f8e30cbe3300d42c129bb
EgorovM <egorov_michil@mail.ru>
2022-10-04 16:54:42 +0300

    [feat] access token life

--- django_core/django_core/settings.py
+++ django_core/django_core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import datetime
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,7 +98,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=15),
 }
 
 # Database
