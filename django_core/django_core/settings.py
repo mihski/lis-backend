68574859c8c86a135e02682a70673224860c2745
MaxBstr <maksimbastrikin@bk.ru>
2022-10-14 03:00:17 +0300

    [feat] Configured serving frontend static

--- django_core/django_core/settings.py
+++ django_core/django_core/settings.py
@@ -218,8 +218,8 @@ CONN_MAX_AGE = None
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/django_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'django_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
