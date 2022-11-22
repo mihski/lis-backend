74b16290c07e60081953bed36552e3b4339b55bd
Michil Egorov <egorov_michil@mail.ru>
2022-11-23 00:58:04 +0300

    [feat] entrypoint to file (#82)
    Co-authored-by: Michil Egorov <michilegorov@Michils-MacBook-Pro.local>

---
+++ django_core/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 manage.py migrate
+
+python3 manage.py collectstatic --noinput
+gunicorn -c gunicorn.config.py django_core.wsgi:application
+
+python3 manage.py runserver
---
+++ django_core/gunicorn.config.py
@@ -0,0 +1,6 @@
+import os
+
+bind = f":{os.getenv('SERVER_BIND_PORT', 8000)}"
+workers = os.getenv("GUNICORN_WORKERS", 1)
+timeout = 30
+reload = True
