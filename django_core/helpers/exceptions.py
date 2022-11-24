7f38779fa1a71549839b8117da3c907e383d43e1
Michil Egorov <michilegorov@Michils-MacBook-Pro.local>
2022-11-24 12:48:09 +0300

    [feat] logging error_code

--- django_core/helpers/exceptions.py
+++ django_core/helpers/exceptions.py
@@ -1,11 +1,17 @@
+import logging
+
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
 from rest_framework.exceptions import APIException
+
+logger = logging.Logger(__name__)
 
 
 def custom_exception_handler(exception: Exception, context: dict) -> Response:
     response: Response = exception_handler(exception, context)
     if response is not None and isinstance(exception, APIException):
         response.data["error_code"] = exception.get_codes()
+
+    logger.warning(str(response.data))
 
     return response
