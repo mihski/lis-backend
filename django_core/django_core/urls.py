9cf7068c29dd50febbd8c3dedb7788405c80f936
Artem <artemsam23@gmail.com>
2024-02-15 23:16:27 +0300

    trying to fix secure headers

--- django_core/django_core/urls.py
+++ django_core/django_core/urls.py
@@ -13,7 +13,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     url=f"https://{settings.HOST}/api",
-    permission_classes=(permissions.IsAdminUser,),
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
