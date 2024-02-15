bb4009fa0d7253dbd9f8ed984850df7ea838b4a8
Artem <artemsam23@gmail.com>
2024-02-15 22:24:54 +0300

    trying to fix secure headers

--- django_core/django_core/urls.py
+++ django_core/django_core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+from django.conf import settings
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -9,6 +10,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title="Life-in-Science API",
       default_version='v1',
+      url=f"https://{settings.HOST}/api",
       contact=openapi.Contact(email="egorov_michil@mail.ru"),
    ),
    public=True,
